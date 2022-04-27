289fdc84a62f8d16e5d3c347f5c597809e04f062
Alfo07 <ammiratialfonso07@gmail.com>
2022-04-27 18:49:28 +0200

    Struttura

---
+++ note.js
@@ -0,0 +1,9 @@
+//Replicare la grafica inserendo a sinistra la barra di ricerca e l'anteprima delle chat e a destra l'interno della chat con i messaggi inviati verdi e quelli ricevuti bianchi
+//Creare una funzione per la visualizzazione dinamica delle chat (tramite v-for)
+//Creare una funzione per mostrare nell'anteprima delle chat una parte della discussione (tramite v-for)
+//Creare una funzione che al click su una conversazione a sinistra, si apre la chat a destra
+//Creare una funzione che permette di scrivere un messaggio nella barra inferiore a destra, e che premendo il tasto enter, il messaggio venga 'inviato'
+//Creare una funzione che fa comparire la risposta dell'interlocutore (ok), dopo uno/due secondi
+//Creare una funzione per la ricerca dei nominativi tra le chat (digitando nome e cognome, sia con lettere maiuscole che minuscole sia parziale, nel campo di ricerca in alto a sinistra)
+//Creare una funzione che permette di cancellare i messaggi (premendo su un triangolino di fianco al messaggio)
+//Creare una funzione per visualizzare l'ora e la data sotto a ogni messaggio e nella parte superiore dove c'è l'immagine e il nome del contatto (Usare la libreria day.js)
